baf92405f4c53427c1889d54ecb0cbc84a6b4f48
Devanxh <73755529+Devanxh@users.noreply.github.com>
2022-09-29 21:15:14 +0530

    first commit

---
+++ getdate.js
@@ -0,0 +1,16 @@
+//
+// console.log(module);
+
+module.exports.getDate = function() {
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+return today.toLocaleDateString("en-US", options);
+
+}
